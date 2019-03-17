4c0845b1b9a312c75713f09df768c79ffefb5e0e
Maurice Shalam <mss619@nyu.edu>
2019-03-17 19:47:02 -0400

    added video walkthrough

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -29,6 +29,15 @@ const AuthForm = props => {
           </div>
           {error && error.response && <div> {error.response.data} </div>}
         </form>
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/qstx2HUHiAU"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </Container>
     </div>
   )
